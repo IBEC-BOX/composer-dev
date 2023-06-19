909d71032c2b58280d440b13f906b5f1b0344af1
Anastas Mironov <ast@l80.ru>
2023-06-19 17:33:40 +0600

    feat: add composer-local command

---
+++ composer-local.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PACKAGES_PATH='$PWD/composer-packages'
+REPOSITORIES="{
+    \"type\": \"path\",
+    \"url\": \"$PACKAGES_PATH/*/*\",
+    \"options\": {
+        \"symlink\": true
+    }
+}"
+
+cd `pwd`
+if [ -f composer.json ]; then
+  jq ".repositories[0] = $REPOSITORIES" composer.json > composer.local.json
+  COMPOSER=composer.local.json composer "$@"
+else
+  echo "File \"composer.json\" does not exist."
+fi
--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ curl -sO https://raw.githubusercontent.com/ast21/composer-dev/main/composer-dev.
 mv composer-dev.sh /usr/local/bin/composer-dev
 chmod +x /usr/local/bin/composer-dev
 
+curl -sO https://raw.githubusercontent.com/ast21/composer-dev/main/composer-local.sh
+mv composer-local.sh /usr/local/bin/composer-local
+chmod +x /usr/local/bin/composer-local
+
 echo "composer-dev command success installed."
+echo "composer-local command success installed."
